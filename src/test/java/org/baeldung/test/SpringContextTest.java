3b4ae3205acd0fbb337206d230f91c1842e06c3e
alex-semenyuk <alexsemenyuk88@gmail.com>
2017-12-20 11:28:37 +0300

    Added spring context test

---
+++ src/test/java/org/baeldung/test/SpringContextTest.java
@@ -0,0 +1,26 @@
+package org.baeldung.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class SpringContextTest implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Test
+    public void checkContext () {
+        assertNotNull(applicationContext);
+    }
+}
