84c5ac2f2dfcca7c2e97bbd9501172b19aeaa992
alex-semenyuk <alexsemenyuk88@gmail.com>
2017-12-21 15:46:59 +0300

    Update spring context test

--- src/test/java/org/baeldung/test/SpringContextTest.java
+++ src/test/java/org/baeldung/test/SpringContextTest.java
@@ -5,11 +5,13 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class SpringContextTest implements ApplicationContextAware {
 
     private ApplicationContext applicationContext;
